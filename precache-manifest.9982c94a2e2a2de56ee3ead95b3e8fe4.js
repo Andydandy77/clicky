15c78a64a912e272d3c52cd354dddf9ab2ce201d
Dave <david.j.anderson94@gmail.com>
2019-01-09 21:35:56 -0800

    Updates

---
+++ precache-manifest.9982c94a2e2a2de56ee3ead95b3e8fe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33a83afb110e60d45794",
+    "url": "/clicky/static/css/main.632d2a4c.chunk.css"
+  },
+  {
+    "revision": "33a83afb110e60d45794",
+    "url": "/clicky/static/js/main.33a83afb.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "9e53dddbe16ca2509119",
+    "url": "/clicky/static/js/runtime~main.9e53dddb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd7753f1ccffabe36d3844d57edcd098",
+    "url": "/clicky/index.html"
+  }
+];
